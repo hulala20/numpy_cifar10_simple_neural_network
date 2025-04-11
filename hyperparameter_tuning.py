c38fb827f9da4d3bd220f3a2cdd7a15ed8ce83b3
hulala20 <107999416+hulala20@users.noreply.github.com>
2025-04-11 13:58:38 +0800

    Update hyperparameter_tuning.py

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,15 +1,16 @@
 from network import NeuralNetwork
-from train import train
+from train import train, load_model_weights
 from test import test
 from plot import plot_curves
 
 
 def hyperparameter_tuning(X_train, y_train, X_val, y_val):
     learning_rates = [0.01, 0.005]
-    hidden_layers_sizes = [[128], [128, 256]]
+    hidden_layers_sizes = [[32], [64], [64, 128]]
     regularization_strengths = [0.0, 0.01]
     best_hyper = {}
     min_val_loss = 1e10
+    i = 1
 
     for lr in learning_rates:
         for hidden in hidden_layers_sizes:
@@ -24,6 +25,8 @@ def hyperparameter_tuning(X_train, y_train, X_val, y_val):
                     best_hyper['learning_rates'] = lr
                     best_hyper['hidden_layers_sizes'] = hidden
                     best_hyper['regularization_strengths'] = reg
+                model.weights = load_model_weights()
                 acc = test(model, X_val, y_val)
-                plot_curves(train_losses=training_losses, val_losses=val_losses, val_accuracies=val_acc)
+                plot_curves(train_losses=training_losses, val_losses=val_losses, val_accuracies=val_acc, filename=f'{i}.png')
+                i += 1
     return best_hyper, training_losses, val_losses, val_acc
